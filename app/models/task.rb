4ff770722f8cca396b467e02e0d9c81dfb01866b
zigzag <zigzag.chen@gmail.com>
2010-03-13 19:43:06 +0800

    Chagned find_all to scope method in Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,7 @@
 class Task < ActiveRecord::Base
   validates_presence_of :name
   has_many :timelogs, :order => 'start_at'
-
-  def self.todos
-    find_all_by_done(false,:order => "priority")
-  end
+  scope :todos, where("done = ?",false).order("priority")
 
   def self.reorder(new_order)
     new_order.each_with_index { |id,index| Task.find(id).update_attribute(:priority,index+1)}
