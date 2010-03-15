99ad412e09b7e7ab38fd4eb1e400ce0f00f49bd9
zigzag <zigzag.chen@gmail.com>
2010-03-15 19:37:15 +0800

    delete useless code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,6 @@
 jQuery(function($) {
     $("#tabs").tabs({
                     ajaxOptions: {
-//                      success: function(xhr,status,index,anchor) {
-//                        refresh();
-//                      },
                       error: function(xhr, status, index, anchor) {
                         $("#notice").html("Couldn't load this tab.");}}
                    });
